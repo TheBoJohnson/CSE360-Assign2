cc11df81c2f22d4b0d94ebb1bc86f2d11a35828f
Bo Johnson <yobo.johnson@gmail.com>
2019-02-22 09:29:54 -0700

    Initial Commit. Also added javadoc comments above all methods

---
+++ src/cse360assign2/Calculator.java
@@ -0,0 +1,76 @@
+/*
+ * Name: Oluwatofarati Johnson
+ * ASU ID: 1211012995
+ * Class ID: 425
+ * Assignmnet2
+ * We are using version control to incrementally update the contents of Calculator.java. After each change we push
+ * a commit to github.
+ */
+
+package cse360assign2;
+
+public class Calculator {
+
+	private int total;
+	
+	/**
+	 * The default constructor for the Calculator class that sets the total member variable to 0 
+	 */
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * Returns the current total value of the calculator
+	 * 
+	 * @return an integer that holds the current total value
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * Adds a specified value to the current total
+	 * 
+	 * @param value the integer value that will be added to the current total
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * Subtracts a specified value to the current total
+	 * 
+	 * @param value the integer value that will be subtracted from the current total
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/**
+	 * Multiplies the current total by a specified value
+	 * 
+	 * @param value the integer value that the total will be multiplied by
+	 */
+	public void multiply (int value) {
+		
+	}
+	
+	/**
+	 * Divides the current total by a specified value
+	 * 
+	 * @param value the integer value that the total will be divied by
+	 */
+	public void divide (int value) {
+		
+	}
+	
+	/**
+	 * Returns a string containing all previous operations and the their arguments in the order they were done in
+	 * 
+	 * @return a string that holds all the previous operations and their arguments
+	 */
+	public String getHistory () {
+		return "";
+	}
+}
